48b8ca273592a31ab1160552d8465c170c56b8e9
gbatistaa <gbatistadev@gmail.com>
2026-01-28 20:06:56 -0300

    fix: update CORS middleware to allow requests from the production frontend URL

--- backend/controllers/main.py
+++ backend/controllers/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # CORS - permitir requisições do frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000, https://mailprismai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
